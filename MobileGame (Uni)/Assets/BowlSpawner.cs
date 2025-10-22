9a30950ac59ec1c7ae153e2a2caa1a4cf4cb2224
Jacklongy <91623130+Jacklongy@users.noreply.github.com>
2025-10-22 23:47:20 +0100

    bowl spawner idea

---
+++ MobileGame (Uni)/Assets/BowlSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Bowl Spawner, changes the way bowls now spawn. I want them to be ready placed on the grid. 
+/// When you merge or take a bowl off its location, a new one will start to spawn there. 30 second timer. 
+/// while the timer is there you cant use that square. 
+/// this allows less randomness, and the player can focus more on food merging. 
+/// give it a try if it doesnt work think of adding a washing up mechanic.
+/// </summary>
+public class BowlSpawner : MonoBehaviour
+{
+    // I need access to the grid. 
+    // i need a timer. and a ring for the Ui
+    // and i need reference to the bowl prefab. 
+
+}
